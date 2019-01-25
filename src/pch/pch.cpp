8ee7b524b5dd7602be84722c37715d364d15a28e
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-01-25 08:40:24 +0200

    updated code base

--- src/pch/pch.cpp
+++ src/pch/pch.cpp
@@ -1,3 +1,25 @@
 #include "pch.h"
 
-#include <uc/math/math.h>
+#include <uc/import/anm/animation.h>
+
+#include <uc/import/fbx/animation.h>
+#include <uc/import/fbx/fbx_common.h>
+#include <uc/import/fbx/fbx_common_multi_material_mesh.h>
+#include <uc/import/fbx/fbx_common_transform.h>
+#include <uc/import/fbx/fbx_helpers.h>
+#include <uc/import/fbx/fbx_transform.h>
+#include <uc/import/fbx/fbx_transform_helper.h>
+#include <uc/import/fbx/indexed_mesh.h>
+#include <uc/import/fbx/multi_material_mesh.h>
+#include <uc/import/fbx/skinned_mesh.h>
+
+
+#include <uc/import/geo/indexed_mesh.h>
+#include <uc/import/geo/merge_attributes_indexed_mesh.h>
+#include <uc/import/geo/merge_attributes_skinned_mesh.h>
+#include <uc/import/geo/multi_material_mesh.h>
+#include <uc/import/geo/skinned_mesh.h>
+
+
+
+
